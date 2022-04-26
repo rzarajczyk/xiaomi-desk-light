5f5ede13ea20ad154e4fd5f19e456fde053364c9
Rafal Zarajczyk <rzarajczyk@gmail.com>
2022-04-26 23:17:53 +0200

    Cleanup && using service-bootstrap && GitHub Actions docker build

--- docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TAG=$(date '+%Y%m%d')
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-NAME=$(basename "$SCRIPT_DIR")
-docker build -t rzarajczyk/$NAME:$TAG .
-docker tag rzarajczyk/$NAME:$TAG rzarajczyk/$NAME:latest
-docker push rzarajczyk/$NAME:$TAG
-docker push rzarajczyk/$NAME:latest
