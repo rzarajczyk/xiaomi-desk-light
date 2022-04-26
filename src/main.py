469c667520e66b73b6bb0c0f5f35feca4266dbdc
Rafal Zarajczyk <rzarajczyk@gmail.com>
2022-04-26 23:26:17 +0200

    service-bootstrap fix

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ from homie_helpers import MqttSettings
 from XiaomiDeskLight import XiaomiDeskLight
 from bootstrap.bootstrap import start_service
 
-config, logger = start_service()
+config, logger, timezone = start_service()
 
-scheduler = BlockingScheduler(timezone=config['timezone'])
+scheduler = BlockingScheduler(timezone=timezone)
 
 device = XiaomiDeskLight(config=config['xiaomi-desk-light'], mqtt_settings=MqttSettings.from_dict(config['mqtt']))
 
